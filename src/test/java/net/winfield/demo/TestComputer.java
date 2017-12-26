04273d56834c2d02f63ad6af5075487468fa5b24
Wenhui Feng <whfeng@CNwhfeng.local>
2017-12-26 13:52:12 +0800

    Change return type

--- src/test/java/net/winfield/demo/TestComputer.java
+++ src/test/java/net/winfield/demo/TestComputer.java
@@ -8,50 +8,50 @@ public class TestComputer {
     @Test
     public void testPlusOneOne_ShouldReturnTwo() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.plus(1, 1);
-        Assert.assertEquals(result, 2.0, 0);
+        String result = computerService.plus(1, 1);
+        Assert.assertEquals(result, "2.0");
     }
 
     @Test
     public void testPlusOneMinusOne_ShouldReturnZero() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.plus(1, -1);
-        Assert.assertEquals(result, 0.0, 0);
+        String result = computerService.plus(1, -1);
+        Assert.assertEquals(result, "0.0");
     }
 
     @Test
     public void testMinusOneOne_ShouldReturnZero() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.minus(1, 1);
-        Assert.assertEquals(result, 0.0, 0);
+        String result = computerService.minus(1, 1);
+        Assert.assertEquals(result, "0.0");
     }
 
     @Test
     public void testMinusOneMinusOne_ShouldReturnTwo() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.minus(1, -1);
-        Assert.assertEquals(result, 2.0, 0);
+        String result = computerService.minus(1, -1);
+        Assert.assertEquals(result, "2.0");
     }
 
     @Test
     public void testMinusOneMinusTwo_ShouldReturnMinusOne() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.minus(1, 2);
-        Assert.assertEquals(result, -1.0, 0);
+        String result = computerService.minus(1, 2);
+        Assert.assertEquals(result, "-1.0");
     }
 
     @Test
     public void testMultiTwoThree_ShouldReturnSix() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.multiple(2, 3);
-        Assert.assertEquals(result, 6.0, 0);
+        String result = computerService.multiple(2, 3);
+        Assert.assertEquals(result, "6.0");
     }
 
     @Test
     public void testMultiTwoMinusThree_ShouldReturnMinusSix() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.multiple(2, -3);
-        Assert.assertEquals(result, -6.0, 0);
+        String result = computerService.multiple(2, -3);
+        Assert.assertEquals(result, "-6.0");
     }
 
     @Test
@@ -85,42 +85,42 @@ public class TestComputer {
     @Test
     public void testModOneOne_ShouldReturnZero() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.mod(1, 1);
-        Assert.assertEquals(result, 0.0, 0);
+        String result = computerService.mod(1, 1);
+        Assert.assertEquals(result, "0.0");
     }
 
     @Test
     public void testModThreeTwo_ShouldReturnOne() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.mod(3, 2);
-        Assert.assertEquals(result, 1.0, 0);
+        String result = computerService.mod(3, 2);
+        Assert.assertEquals(result, "1.0");
     }
 
     @Test
     public void testModThreeMinusTwo_ShouldReturnOne() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.mod(3, -2);
-        Assert.assertEquals(result, 1.0, 0);
+        String result = computerService.mod(3, -2);
+        Assert.assertEquals(result, "1.0");
     }
 
     @Test
     public void testModFourTwo_ShouldReturnZero() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.mod(4, 2);
-        Assert.assertEquals(result, 0.0, 0);
+        String result = computerService.mod(4, 2);
+        Assert.assertEquals(result, "0.0");
     }
 
     @Test
     public void testSquareTwo_ShouldReturnFour() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.square(2);
-        Assert.assertEquals(result, 4.0, 0);
+        String result = computerService.square(2);
+        Assert.assertEquals(result, "4.0");
     }
 
     @Test
     public void testSquareMinusTwo_ShouldReturnFour() {
         ComputerService computerService = new ComputerService();
-        float result = computerService.square(-2);
-        Assert.assertEquals(result, 4.0, 0);
+        String result = computerService.square(-2);
+        Assert.assertEquals(result, "4.0");
     }
 }
